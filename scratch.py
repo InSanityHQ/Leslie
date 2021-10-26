11f61da9bcf8a0861ddd5d4643a8e3a099fc9c84
Houjun Liu <hliu@shabang.cf>
2021-10-25 20:23:41 -0700

    wandb

--- scratch.py
+++ scratch.py
@@ -14,21 +14,38 @@ from transformers import AdamW
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.distributions import Categorical
 
 import numpy as np
 import re
 
 from tqdm import tqdm
 
+import wandb
+
 print("Hello and welcome. Let's get started.")
 
 # Parametres
-MAX_LENGTH = 50
-ACTOR_LR = 1e-4
-CRITIC_LR = 1e-4
-BATCH_SIZE = 16
-EPOCHS = 20
+
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+hyperparametre_defaults = dict(
+    max_length = 50,
+    actor_lr = 5e-5,
+    critic_lr = 1e-3,
+    batch_size = 16,
+    epochs = 20
+)
+
+run = wandb.init(entity='inscriptio', project='leslie', config=hyperparametre_defaults)
+# run = wandb.init(entity='inscriptio', project='leslie', config=hyperparametre_defaults, mode="disabled")
+config = wandb.config
+
+MAX_LENGTH = config.max_length
+ACTOR_LR = config.actor_lr
+CRITIC_LR = config.critic_lr
+BATCH_SIZE = config.batch_size
+EPOCHS = config.epochs
 
 # Simplicity evalutaion
 def unique_words(sentence):
@@ -85,24 +102,22 @@ bart_model = BartForConditionalGeneration.from_pretrained("facebook/bart-base", 
 
 print("Creating critic model.")
 class Critic(nn.Module):
-    def __init__(self, vocab_size, max_length):
+    def __init__(self, max_length):
         super(Critic,self).__init__()
 
-        self.d1 = nn.Linear(vocab_size, 1024)
-        self.d2 = nn.Linear(1024, 512)
-        self.d3 = nn.Linear(512, 32)
-        self.flatten = nn.Flatten()
-        self.d4 = nn.Linear(32*max_length, 1)
+        self.batchnorm = nn.BatchNorm1d(max_length)
+        self.d1 = nn.Linear(max_length, 32)
+        self.d2 = nn.Linear(32, 16)
+        self.d3 = nn.Linear(16, 1)
 
     def forward(self,x):
+        x = self.batchnorm(x)
         x = F.relu(self.d1(x))
         x = F.relu(self.d2(x))
-        x = F.relu(self.d3(x))
-        x = F.relu(self.flatten(x))
-        x = F.tanh(self.d4(x))
+        x = F.tanh(self.d3(x))
         return x
 
-critic_model = Critic(bart_tokenizer.vocab_size, MAX_LENGTH)
+critic_model = Critic(MAX_LENGTH)
 
 np2tens = lambda x: torch.tensor(x)
 
@@ -115,13 +130,16 @@ critic_model.to(DEVICE)
 critic_model.train()
 critic_optim = AdamW(critic_model.parameters(), lr=CRITIC_LR)
 
+run.watch(bart_model)
+run.watch(critic_model)
+
 print("Instantiating similarity model.")
 similarity_model = SentenceTransformer('stsb-bert-base')
 
 print("Starting to train.")
 for _ in range(EPOCHS):
     dataset_bar = tqdm(range(0, len(dataset_train)-BATCH_SIZE, BATCH_SIZE))
-    for batch_id in dataset_bar:
+    for i, batch_id in enumerate(dataset_bar):
         batch = dataset_train[batch_id:batch_id+BATCH_SIZE]
 
         # Encode each sentence 
@@ -135,7 +153,7 @@ for _ in range(EPOCHS):
         result = bart_model(sentence_padded, attention_mask=sentence_mask)
         # Logit-shame the state. We support logit-shaming
         # to figure out what the value of the state is 
-        values = torch.flatten(critic_model(F.one_hot(sentence_padded, num_classes=bart_tokenizer.vocab_size).float()))
+        values = torch.flatten(critic_model(sentence_padded.float()))
         # Get the logits
         logits = result["logits"]
 
@@ -146,19 +164,21 @@ for _ in range(EPOCHS):
         # Return the final string
         logits_string = [re.sub("<s>", "", i.split("</s>")[0]) for i in logits_string]
         # Calculate reward value for these strings
-        rewards = np2tens([reward(i,j,0.5,similarity_model,0.7) for i,j in zip(batch, logits_string)]).to(DEVICE)
+        rewards = np2tens([reward(i,j,0.5,similarity_model,0.5) for i,j in zip(batch, logits_string)]).to(DEVICE)
 
         # Calculate the advantages of the model
         advantages = rewards - values
 
         # Calculate the log probabilites of the model output by softmaxing it
-        logits_log_probs = F.softmax(logits, dim=2).log()
+        logits_probs = F.softmax(logits, dim=2)
 
-        # Gather the probability values of the selected actions
-        action_log_probs = logits_log_probs.gather(2, torch.unsqueeze(actions, 2))
+        # Gather the log probability values of the selected actions
+        action_log_probs = logits_probs.gather(2, torch.unsqueeze(actions, 2)).log()
 
         # Add em up, multiply by the advantage, and calculate the mean
-        actor_loss = -(advantages.detach()*action_log_probs).mean()
+        actor_loss = torch.stack(
+                        [a*l for a,l in zip(advantages.detach(),action_log_probs)]
+                     ).mean() * -1
         # The critic wants to match the actual rewards as much as possible
         # So it's just MSE loss 
         critic_loss = advantages.pow(2).mean()
@@ -170,6 +190,15 @@ for _ in range(EPOCHS):
 
         # Add the losses
         loss = actor_loss+critic_loss
+
+        if i % 50 == 0:
+            run.log({"actor_loss": actor_loss.item(),
+                     "critic_loss": critic_loss.item(),
+                     "advantage": advantages.mean().item(),
+                     "loss": loss.item(),
+                     "reward": rewards[0].item(),
+                     "sample": wandb.Html(logits_string[0])})
+
         # Backprop!
         loss.backward()
 
