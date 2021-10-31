27392e5c884fc29adcab7560250c99437d985fc6
Houjun Liu <hliu@shabang.cf>
2021-10-31 13:29:34 -0700

    getting rid of the critic

--- scratch.py
+++ scratch.py
@@ -31,8 +31,8 @@ DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 
 hyperparametre_defaults = dict(
     max_length = 50,
-    actor_lr = 1e-6,
-    critic_lr = 5e-6,
+    actor_lr = 1e-5,
+    # critic_lr = 5e-5,
     batch_size = 16,
     epochs = 1000
 )
@@ -43,7 +43,7 @@ config = wandb.config
 
 MAX_LENGTH = config.max_length
 ACTOR_LR = config.actor_lr
-CRITIC_LR = config.critic_lr
+# CRITIC_LR = config.critic_lr
 BATCH_SIZE = config.batch_size
 EPOCHS = config.epochs
 
@@ -68,20 +68,21 @@ def semantic_similarity(a,b,model):
 
 # Mixed simplification reward 
 def reward(src,tgt,reduce_factor,model,similarity_mix=0.5):
-    scaled_similarity = similarity_mix*semantic_similarity(src,tgt,model)
+    # scaled_similarity = semantic_similarity(src,tgt,model)
 
-    a = unique_words(src)
-    b = unique_words(tgt)
-    target_vocab_size = a*reduce_factor
+    # a = unique_words(src)
+    # b = unique_words(tgt)
+    # target_vocab_size = a*reduce_factor
 
-    try: 
-        vocab_reduction = 1-abs(b-target_vocab_size)/b
-        scaled_vocab_reduction = vocab_reduction *(1-similarity_mix)
-    except ZeroDivisionError:
-        scaled_vocab_reduction = 0
+    # try: 
+    #     vocab_reduction = 1-abs(b-target_vocab_size)/b
+    #     scaled_vocab_reduction = vocab_reduction *(1-similarity_mix)
+    # except ZeroDivisionError:
+    #     scaled_vocab_reduction = 0
 
            # subtract by 0.5 to be able to distribute negative reward
-    return (scaled_similarity+scaled_vocab_reduction)-0.5
+    # return (scaled_similarity+scaled_vocab_reduction)-0.5
+    return semantic_similarity(src,tgt,model)
 
     # return semantic_similarity(src,tgt,model)
 
@@ -100,24 +101,24 @@ bart_config = BartConfig.from_pretrained("facebook/bart-base")
 bart_tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
 bart_model = BartForConditionalGeneration.from_pretrained("facebook/bart-base", config=bart_config)
 
-print("Creating critic model.")
-class Critic(nn.Module):
-    def __init__(self, max_length):
-        super(Critic,self).__init__()
+# print("Creating critic model.")
+# class Critic(nn.Module):
+#     def __init__(self, max_length):
+#         super(Critic,self).__init__()
 
-        self.batchnorm = nn.BatchNorm1d(max_length)
-        self.d1 = nn.Linear(max_length, 32)
-        self.d2 = nn.Linear(32, 16)
-        self.d3 = nn.Linear(16, 1)
+#         self.batchnorm = nn.BatchNorm1d(max_length)
+#         self.d1 = nn.Linear(max_length, 32)
+#         self.d2 = nn.Linear(32, 16)
+#         self.d3 = nn.Linear(16, 1)
 
-    def forward(self,x):
-        x = self.batchnorm(x)
-        x = F.tanh(self.d1(x))
-        x = F.tanh(self.d2(x))
-        x = F.tanh(self.d3(x))
-        return x
+#     def forward(self,x):
+#         x = self.batchnorm(x)
+#         x = F.tanh(self.d1(x))
+#         x = F.tanh(self.d2(x))
+#         x = F.tanh(self.d3(x))
+#         return x
 
-critic_model = Critic(MAX_LENGTH)
+# critic_model = Critic(MAX_LENGTH)
 
 np2tens = lambda x: torch.tensor(x)
 
@@ -126,11 +127,12 @@ bart_model.to(DEVICE)
 bart_model.train()
 actor_optim = AdamW(bart_model.parameters(), lr=ACTOR_LR)
 
-critic_model.to(DEVICE)
-critic_model.train()
-critic_optim = AdamW(critic_model.parameters(), lr=CRITIC_LR)
+# critic_model.to(DEVICE)
+# critic_model.train()
+# critic_optim = AdamW(critic_model.parameters(), lr=CRITIC_LR)
 
-run.watch((bart_model, critic_model))
+# run.watch((bart_model, critic_model))
+run.watch(bart_model)
 
 print("Instantiating similarity model.")
 similarity_model = SentenceTransformer('stsb-bert-base')
@@ -152,7 +154,7 @@ for _ in range(EPOCHS):
         result = bart_model(sentence_padded, attention_mask=sentence_mask)
         # Logit-shame the state. We support logit-shaming
         # to figure out what the value of the state is 
-        values = torch.flatten(critic_model(sentence_padded.float()))
+        # values = torch.flatten(critic_model(sentence_padded.float()))
         # Get the logits
         logits = result["logits"]
 
@@ -165,44 +167,44 @@ for _ in range(EPOCHS):
         # Calculate reward value for these strings
         rewards = np2tens([reward(i,j,0.5,similarity_model,0.5) for i,j in zip(batch, logits_string)]).to(DEVICE)
 
-        # Calculate the advantages of the model
-        advantages = rewards - values
-
-        # Calculate the log probabilites of the model output by softmaxing it
-        logits_probs = F.softmax(logits, dim=2)
+        # # Calculate the advantages of the model
+        # advantages = rewards - values
 
         # Gather the log probability values of the selected actions
-        action_log_probs = logits_probs.gather(2, torch.unsqueeze(actions, 2)).log()
+        action_log_probs = torch.sum(logits.gather(2, torch.unsqueeze(actions, 2)), dim=1).log()
 
         # Add em up, multiply by the advantage, and calculate the mean
         actor_loss = torch.stack(
-                        [a*l for a,l in zip(advantages.detach(),action_log_probs)]
+                        [a*l for a,l in zip(rewards,action_log_probs)]
                      ).mean() * -1
+
         # The critic wants to match the actual rewards as much as possible
         # So it's just MSE loss 
-        critic_loss = advantages.pow(2).mean()
+        # critic_loss = advantages.pow(2).mean()
 
-        dataset_bar.set_description(f"Sample: {batch_id}, Actor: {round(actor_loss.item(),3)}, Critic: {round(critic_loss.item(),3)}, Reward: {round(rewards[0].item(),3)}")
+        # dataset_bar.set_description(f"Sample: {batch_id}, Actor: {round(actor_loss.item(),3)}, Critic: {round(critic_loss.item(),3)}, Reward: {round(rewards[0].item(),3)}")
+        dataset_bar.set_description(f"Sample: {batch_id}, Actor: {round(actor_loss.item(),3)}, Reward: {round(rewards[0].item(),3)}")
 
         if batch_id % 500 == 0:
             print(f"Sample: {batch_id}; Output: {logits_string[0]}")
 
         # Add the losses
-        loss = actor_loss+critic_loss
+        # loss = actor_loss+critic_loss
 
         if i % 50 == 0:
             run.log({"actor_loss": actor_loss.item(),
-                     "critic_loss": critic_loss.item(),
-                     "advantage": advantages.mean().item(),
-                     "loss": loss.item(),
+                     # "critic_loss": critic_loss.item(),
+                     # "advantage": advantages.mean().item(),
+                     # "loss": loss.item(),
                      "reward": rewards[0].item(),
                      "sample": wandb.Html(logits_string[0])})
 
         # Backprop!
-        loss.backward()
+        # loss.backward()
+        actor_loss.backward()
 
-        critic_optim.step()
-        critic_optim.zero_grad()
+        # critic_optim.step()
+        # critic_optim.zero_grad()
 
         actor_optim.step()
         actor_optim.zero_grad()
