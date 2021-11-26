cdb2bc73fa0359acb1e11592b64d057f787141b7
Houjun Liu <hliu@shabang.cf>
2021-11-25 16:29:43 -0800

    wait but  maybe we /are/ chasing the instability

--- scratch.py
+++ scratch.py
@@ -37,8 +37,7 @@ hyperparametre_defaults = dict(
     actor_lr = 5e-5,
     batch_size = 16,
     epochs = 1000,
-    critic_lr = 5e-6,
-    replay_buffer = 50
+    replay_buffer = 100
 )
 
 run = wandb.init(entity='inscriptio', project='leslie', config=hyperparametre_defaults)
@@ -47,7 +46,7 @@ config = wandb.config
 
 MAX_LENGTH = config.max_length
 ACTOR_LR = config.actor_lr
-CRITIC_LR = config.critic_lr
+# CRITIC_LR = config.critic_lr
 BATCH_SIZE = config.batch_size
 EPOCHS = config.epochs
 REPLAY = config.replay_buffer
@@ -90,15 +89,15 @@ def reward(src,tgt,model):
     try: 
         usage_src = sum([usage[i] for i in words_src])/len(words_src)
         usage_tgt = sum([usage[i] for i in words_tgt])/len(words_tgt)
-        simplification_rating = ((usage_tgt-usage_src)/usage_src)-0.5
+        simplification_rating = ((usage_tgt-usage_src)/usage_src)
     except ZeroDivisionError:
         simplification_rating = -2
 
     scaled_similarity = semantic_similarity(src,tgt,model)
-    similarity_rating = scaled_similarity - 0.5
+    similarity_rating = scaled_similarity
 
                                                       # slight punishment for inaction
-    return (simplification_rating + similarity_rating)
+    return (simplification_rating + similarity_rating)-0.25
 
 print("Instantiating similarity model.")
 similarity_model = SentenceTransformer('stsb-bert-base')
@@ -118,35 +117,12 @@ bart_model = BartForConditionalGeneration.from_pretrained("facebook/bart-base", 
 
 np2tens = lambda x: torch.tensor(x)
 
-print("Creating critic model.")
-class Critic(nn.Module):
-    def __init__(self, max_length):
-        super(Critic,self).__init__()
-
-        self.batchnorm = nn.BatchNorm1d(max_length)
-        self.d1 = nn.Linear(max_length, 32)
-        self.d2 = nn.Linear(32, 16)
-        self.d3 = nn.Linear(16, 1)
-
-    def forward(self,x):
-        x = self.batchnorm(x)
-        x = F.tanh(self.d1(x))
-        x = F.tanh(self.d2(x))
-        x = F.tanh(self.d3(x))
-        return x
-
-critic_model = Critic(MAX_LENGTH)
-
 print("Creating optimizers and moving models.")
 bart_model.to(DEVICE)
 bart_model.train()
 actor_optim = AdamW(bart_model.parameters(), lr=ACTOR_LR)
 
-critic_model.to(DEVICE)
-critic_model.train()
-critic_optim = AdamW(critic_model.parameters(), lr=CRITIC_LR)
-
-run.watch((bart_model, critic_model))
+run.watch(bart_model)
 
 print("Starting to train.")
 for _ in range(EPOCHS):
@@ -166,10 +142,6 @@ for _ in range(EPOCHS):
         # Logit-shame the state. We support logit-shaming
         # to figure out what the value of the state is 
 
-        # Logit-shame the state. We support logit-shaming
-        # to figure out what the value of the state is 
-        values = torch.flatten(critic_model(sentence_padded.float()))
-
         # Get the logits
         logits = result["logits"]
 
@@ -183,48 +155,34 @@ for _ in range(EPOCHS):
         rewards = np2tens([reward(i,j,similarity_model) for i,j in zip(batch, logits_string)]).to(DEVICE)
 
         # # Calculate the advantages of the model
-        advantages = rewards - values
+        # advantages = rewards -
 
         # Get the logits' probs by normalizing with softmax
         logits_probs = F.softmax(logits, dim=2)
 
-        # Gather the log probability values of the selected actions
-        action_log_probs = logits_probs.gather(2, torch.unsqueeze(actions, 2)).log()
-
         # Add em up, multiply by the advantage, and calculate the mean
-        actor_loss = (torch.stack([a*l for a,l in zip(advantages.detach(),action_log_probs)]).mean().sum() * -1)
-        # The critic wants to match the actual rewards as much as possible
-        # So it's just MSE loss 
-        critic_loss = advantages.pow(2).mean()
-
-        # Add the losses
-        loss = (actor_loss+critic_loss)/REPLAY
+        actor_loss = (torch.stack([a*l for a,l in zip(rewards,logits_probs.log())]).mean() * -1)/REPLAY
 
         # The critic wants to match the actual rewards as much as possible
         # So it's just MSE loss 
-        dataset_bar.set_description(f"Sample: {batch_id}, Actor: {round(actor_loss.item(),3)}, Critic: {round(critic_loss.item(),3)}, Reward: {round(rewards[0].item(),3)}")
+        dataset_bar.set_description(f"Sample: {batch_id}, Actor: {round(actor_loss.item(),3)}, Reward: {round(rewards[0].item(),3)}")
 
         if i % 50 == 0:
             print(f"Sample: {batch_id}; Output: {logits_string[0]}")
 
         if i % 10 == 0:
             run.log({"actor_loss": actor_loss.item(),
-                     "critic_loss": critic_loss.item(),
-                     "advantage": advantages.mean().item(),
+                     # "critic_loss": critic_loss.item(),
+                     # "advantage": advantages.mean().item(),
                      # "loss": loss.item(),
                      "reward": rewards[0].item(),
                      "sample": wandb.Html(logits_string[0])})
-
-        # Backprop!
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(bart_model.parameters(), 0.1)
-        torch.nn.utils.clip_grad_norm_(critic_model.parameters(), 0.1)
 
         # if we hit the replay buffer, then go ahead and update weights
         if i != 0 and i % REPLAY == 0:
             actor_optim.step()
             actor_optim.zero_grad()
 
-            critic_optim.step()
-            critic_optim.zero_grad()
-
+        # Backprop!
+        actor_loss.backward()
+        torch.nn.utils.clip_grad_norm_(bart_model.parameters(), 0.1)
