e656afce090cb7954063bd57c9bae9aacbab7016
Houjun Liu <hliu@shabang.cf>
2021-11-25 20:31:37 -0800

    I am confusian

--- scratch.py
+++ scratch.py
@@ -37,7 +37,7 @@ hyperparametre_defaults = dict(
     actor_lr = 5e-5,
     batch_size = 16,
     epochs = 1000,
-    replay_buffer = 100
+    replay_buffer = 50
 )
 
 run = wandb.init(entity='inscriptio', project='leslie', config=hyperparametre_defaults)
@@ -96,8 +96,8 @@ def reward(src,tgt,model):
     scaled_similarity = semantic_similarity(src,tgt,model)
     similarity_rating = scaled_similarity
 
-                                                      # slight punishment for inaction
-    return (simplification_rating + similarity_rating)-0.25
+                                                      # rescale "doing nothing" to 0
+    return (simplification_rating + similarity_rating)-1
 
 print("Instantiating similarity model.")
 similarity_model = SentenceTransformer('stsb-bert-base')
@@ -160,8 +160,11 @@ for _ in range(EPOCHS):
         # Get the logits' probs by normalizing with softmax
         logits_probs = F.softmax(logits, dim=2)
 
+        # Gather the log probability values of the selected actions
+        action_log_probs = logits_probs.gather(2, torch.unsqueeze(actions, 2)).log()
+
         # Add em up, multiply by the advantage, and calculate the mean
-        actor_loss = (torch.stack([a*l for a,l in zip(rewards,logits_probs.log())]).mean() * -1)/REPLAY
+        actor_loss = (torch.stack([a*l for a,l in zip(rewards,action_log_probs)]).mean())/REPLAY
 
         # The critic wants to match the actual rewards as much as possible
         # So it's just MSE loss 
